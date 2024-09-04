6740a67b7b2bf19f238abf02a7bfdd01e6ffe266
Nonso <nonsootoh@gmail.com>
2024-09-03 21:48:41 -0400

    chore: adds feature section

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,13 @@ import { Articles } from "../utils/constants/dummyData";
 const Home = () => {
   return (
     <main className={`mainContent ${styles.mainWrapper}`}>
+      <Newsletter
+        categoryTitle={Articles[0].categoryTitle}
+        articleTitle={Articles[0].articleTitle}
+        articleDescription={Articles[0].articleDescription}
+        author={Articles[0].author}
+        featureArticle
+      />
       <h2 className={styles.pageHeader}>Past newsletters</h2>
       <p className={styles.pageSubHeader}>What have we been up too?</p>
 
--- components/Newsletter/Newsletter.tsx
+++ components/Newsletter/Newsletter.tsx
@@ -20,22 +20,31 @@ const Newsletter = ({
   author,
 }: Props) => {
   return (
-    <article className={styles.mainWrapper}>
-      <div className={styles.background}></div>
-      <p className={styles.categoryTitle} date-feature={String(featureArticle)}>
-        {featureArticle ? "Inside the tank" : categoryTitle}
-      </p>
-      <h3 className={styles.articleTitle}>{articleTitle}</h3>
-      <p className={styles.articleDescription}>{articleDescription}</p>
+    <section className={styles.containerWrapper}>
+      <article className={styles.mainWrapper}>
+        <div className={styles.background}></div>
+        <div className={styles.articleMetadata}>
+          <p
+            className={styles.categoryTitle}
+            date-feature={String(featureArticle)}
+          >
+            {featureArticle ? "Inside the tank" : categoryTitle}
+          </p>
+          <h3 className={styles.articleTitle}>{articleTitle}</h3>
+          <p className={styles.articleDescription}>{articleDescription}</p>
 
-      <section className={styles.author}>
-        <div className={styles.authorBackground}></div>
-        <div>
-          <p className={styles.authorItems}>{author.name}</p>
-          <p className={styles.authorItems}>{author.title}</p>
+          {!featureArticle && (
+            <section className={styles.author}>
+              <div className={styles.authorBackground}></div>
+              <div>
+                <p className={styles.authorItems}>{author.name}</p>
+                <p className={styles.authorItems}>{author.title}</p>
+              </div>
+            </section>
+          )}
         </div>
-      </section>
-    </article>
+      </article>
+    </section>
   );
 };
 
