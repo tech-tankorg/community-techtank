be084cd8c0d9cd53ec8f7faef1d6edccf384d683
Nonso <nonsootoh@gmail.com>
2024-09-04 20:13:19 -0400

    chore: adds cloudinary image to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
